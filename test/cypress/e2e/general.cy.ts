612625adaa2feb35d23eab02e302a17deb84f00e
Thomas Winkler <twi@softwareag.com>
2024-01-12 20:53:11 +0100

    Fixed setLanguage static response interception  (#42)
    * Fixed setLanguage interception to pass request to server and update language setting. Replaced static response. Added tests. Improved loading of json http resources for testing.
    * Removed logging

--- test/cypress/e2e/general.cy.ts
+++ test/cypress/e2e/general.cy.ts
@@ -7,10 +7,7 @@ describe("general", () => {
       cy.disableGainsight()
         .as("interception")
         .then(() => {
-          // json extension required to have the correct content-type delivered into
-          // the interception. without res will be string instead of object
-          // might be also possible by using overrideMimeType on jqXHR object
-          return $.get(url(`/tenant/currentTenant.json`));
+          return $.get(url(`/tenant/currentTenant`));
         })
         .then((response) => {
           expect(response.customProperties.gainsightEnabled).to.eq(false);
@@ -30,7 +27,65 @@ describe("general", () => {
         .as("interception")
         .then(() => {
           $.get(url(`/tenant/system/options/gainsight/api.key`));
+        });
+    });
+  });
+
+  context("hideCookieBanner", () => {
+    it("hideCookieBanner should update localStorage cookie value", () => {
+      cy.window().then((win) => {
+        const cookie = win.localStorage.getItem("acceptCookieNotice");
+        expect(cookie).to.be.null;
+      });
+
+      cy.hideCookieBanner();
+
+      cy.window().then((win) => {
+        const cookie = JSON.parse(
+          win.localStorage.getItem("acceptCookieNotice")
+        );
+        expect(cookie).to.deep.eq({ required: true, functional: true });
+      });
+    });
+  });
+
+  context("setLanguage", () => {
+    it("should call setLocale to init language ", () => {
+      const language = "en"; // replace with the language you want to test
+      const setLanguageStub = cy.stub(global, "setLocale");
+      cy.setLanguage(language).then(() => {
+        expect(setLanguageStub).to.be.calledWith(language);
+      });
+    });
+
+    it("should update localStorage to set c8y_language", () => {
+      const language = "en"; // replace with the language you want to test
+      cy.window().then((win) => {
+        const lang = win.localStorage.getItem("c8y_language");
+        expect(lang).to.be.null;
+      });
+
+      cy.setLanguage(language).then(() => {
+        expect(window.localStorage.getItem("c8y_language")).to.eq(language);
+        cy.window().then((win) => {
+          const lang = win.localStorage.getItem("c8y_language");
+          expect(lang).to.deep.eq(language);
+        });
+      });
+    });
+
+    it("should intercept inventory request and update language key", () => {
+      cy.setLanguage("de")
+        .as("interception")
+        .then(() => {
+          return $.get(
+            url(`/inventory/managedObjects?fragmentType=languageXYZ`)
+          );
         })
+        .then((response) => {
+          expect(response.managedObjects[0].languageXYZ).to.eq("de");
+        })
+        .wait("@interception");
     });
   });
 });
