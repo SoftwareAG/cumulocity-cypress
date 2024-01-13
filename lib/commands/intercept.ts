d186ea3d0af5934f7626e1af17f7c3b5f963fab7
Thomas Winkler <twi@softwareag.com>
2024-01-13 12:57:31 +0100

    Add pacts/constants and moved constants definition from intercept.

--- lib/commands/intercept.ts
+++ lib/commands/intercept.ts
@@ -1,3 +1,9 @@
+import {
+  HTTP_METHODS,
+  STATIC_RESPONSE_KEYS,
+  STATIC_RESPONSE_WITH_OPTIONS_KEYS,
+} from "../pacts/constants";
+
 const { _ } = Cypress;
 
 const wrapResponseFunction = (fn: any) => {
@@ -159,54 +165,3 @@ function hasStaticResponseWithOptionsKeys(obj: any) {
       _.isEmpty(obj))
   );
 }
-
-// cypress static response keys, see packages/driver/src/cy/net-stubbing/static-response-utils.ts
-const STATIC_RESPONSE_KEYS = [
-  "body",
-  "fixture",
-  "statusCode",
-  "headers",
-  "forceNetworkError",
-  "throttleKbps",
-  "delay",
-  "delayMs",
-];
-
-const HTTP_METHODS = [
-  "ACL",
-  "BIND",
-  "CHECKOUT",
-  "CONNECT",
-  "COPY",
-  "DELETE",
-  "GET",
-  "HEAD",
-  "LINK",
-  "LOCK",
-  "M-SEARCH",
-  "MERGE",
-  "MKACTIVITY",
-  "MKCALENDAR",
-  "MKCOL",
-  "MOVE",
-  "NOTIFY",
-  "OPTIONS",
-  "PATCH",
-  "POST",
-  "PROPFIND",
-  "PROPPATCH",
-  "PURGE",
-  "PUT",
-  "REBIND",
-  "REPORT",
-  "SEARCH",
-  "SOURCE",
-  "SUBSCRIBE",
-  "TRACE",
-  "UNBIND",
-  "UNLINK",
-  "UNLOCK",
-  "UNSUBSCRIBE",
-];
-
-const STATIC_RESPONSE_WITH_OPTIONS_KEYS = [...STATIC_RESPONSE_KEYS, "log"];
---
+++ lib/pacts/constants.ts
@@ -0,0 +1,53 @@
+// cypress static response keys, see packages/driver/src/cy/net-stubbing/static-response-utils.ts
+export const STATIC_RESPONSE_KEYS = [
+  "body",
+  "fixture",
+  "statusCode",
+  "headers",
+  "forceNetworkError",
+  "throttleKbps",
+  "delay",
+  "delayMs",
+];
+
+export const HTTP_METHODS = [
+  "ACL",
+  "BIND",
+  "CHECKOUT",
+  "CONNECT",
+  "COPY",
+  "DELETE",
+  "GET",
+  "HEAD",
+  "LINK",
+  "LOCK",
+  "M-SEARCH",
+  "MERGE",
+  "MKACTIVITY",
+  "MKCALENDAR",
+  "MKCOL",
+  "MOVE",
+  "NOTIFY",
+  "OPTIONS",
+  "PATCH",
+  "POST",
+  "PROPFIND",
+  "PROPPATCH",
+  "PURGE",
+  "PUT",
+  "REBIND",
+  "REPORT",
+  "SEARCH",
+  "SOURCE",
+  "SUBSCRIBE",
+  "TRACE",
+  "UNBIND",
+  "UNLINK",
+  "UNLOCK",
+  "UNSUBSCRIBE",
+];
+
+export const STATIC_RESPONSE_WITH_OPTIONS_KEYS = [
+  ...STATIC_RESPONSE_KEYS,
+  "log",
+];
