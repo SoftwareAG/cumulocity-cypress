5ade8f27357f0c01d458994fa6381e4397fe1cd8
Christian Guether <christian.guether@softwareag.com>
2024-02-09 16:25:00 +0100

    #59 add tests for clearUserRoles and assignUserRoles commands

--- test/cypress/e2e/administration.cy.ts
+++ test/cypress/e2e/administration.cy.ts
@@ -1,8 +1,5 @@
-import {
-  expectC8yClientRequest,
-  initRequestStub,
-  stubResponses,
-} from "../support/util";
+import { IUserGroup } from '@c8y/client';
+import { expectC8yClientRequest, initRequestStub, stubResponses } from '../support/util';
 const { _, sinon } = Cypress;
 
 declare global {
@@ -11,126 +8,297 @@ declare global {
   }
 }
 
-describe("administration", () => {
+describe('administration', () => {
   beforeEach(() => {
-    Cypress.env("C8Y_USERNAME", undefined);
-    Cypress.env("C8Y_PASSWORD", undefined);
-    Cypress.env("C8Y_TENANT", undefined);
-    Cypress.env("C8Y_PLUGIN_LOADED", undefined);
+    Cypress.env('C8Y_USERNAME', undefined);
+    Cypress.env('C8Y_PASSWORD', undefined);
+    Cypress.env('C8Y_TENANT', undefined);
+    Cypress.env('C8Y_PLUGIN_LOADED', undefined);
 
     initRequestStub();
     stubResponses([
-      new window.Response(JSON.stringify({ name: "t1234" }), {
+      new window.Response(JSON.stringify({ name: 't1234' }), {
         status: 200,
-        statusText: "OK",
-        headers: { "content-type": "application/json" },
+        statusText: 'OK',
+        headers: { 'content-type': 'application/json' },
       }),
     ]);
   });
 
-  context("deleteUser", () => {
-    it("should delete user from user options", function () {
+  context('deleteUser', () => {
+    it('should delete user from user options', function () {
       stubResponses([
         new window.Response(null, {
           status: 204,
-          statusText: "OK",
-          headers: { "content-type": "application/json" },
+          statusText: 'OK',
+          headers: { 'content-type': 'application/json' },
         }),
       ]);
 
-      cy.getAuth({ user: "admin", password: "mypassword", tenant: "t12345678" })
-        .deleteUser({ userName: "test", displayName: "wewe" })
+      cy.getAuth({ user: 'admin', password: 'mypassword', tenant: 't12345678' })
+        .deleteUser({ userName: 'test', displayName: 'wewe' })
         .then((response) => {
           expect(response.status).to.eq(204);
           expectC8yClientRequest({
             url: `${Cypress.config().baseUrl}/user/t12345678/users/test`,
             auth: {
-              user: "admin",
-              password: "mypassword",
-              tenant: "t12345678",
+              user: 'admin',
+              password: 'mypassword',
+              tenant: 't12345678',
             },
             headers: { UseXBasic: true },
-            method: "DELETE",
+            method: 'DELETE',
           });
         });
     });
 
-    it("should use client options", function () {
+    it('should use client options', function () {
       stubResponses([
         new window.Response(null, {
           status: 404,
-          statusText: "OK",
-          headers: { "content-type": "application/json" },
+          statusText: 'OK',
+          headers: { 'content-type': 'application/json' },
         }),
       ]);
-      cy.getAuth({ user: "admin", password: "mypassword", tenant: "t12345678" })
+      cy.getAuth({ user: 'admin', password: 'mypassword', tenant: 't12345678' })
         .deleteUser(
-          { userName: "test", displayName: "wewe" },
-          { baseUrl: "https://abc.def.com", failOnStatusCode: true }
+          { userName: 'test', displayName: 'wewe' },
+          { baseUrl: 'https://abc.def.com', failOnStatusCode: true }
         )
         .then((response) => {
           expect(response.status).to.eq(404);
           expectC8yClientRequest({
             url: `https://abc.def.com/user/t12345678/users/test`,
             auth: {
-              user: "admin",
-              password: "mypassword",
-              tenant: "t12345678",
+              user: 'admin',
+              password: 'mypassword',
+              tenant: 't12345678',
             },
             headers: { UseXBasic: true },
-            method: "DELETE",
+            method: 'DELETE',
           });
         });
     });
 
-    it("throws error for missing user and logs username", (done) => {
-      Cypress.once("fail", (err) => {
-        expect(err.message).to.contain("Missing argument. Requiring IUser");
-        expect(Cypress.log).to.be.calledWithMatch(
-          sinon.match({ message: `{user: test}` })
-        );
+    it('throws error for missing user and logs username', (done) => {
+      Cypress.once('fail', (err) => {
+        expect(err.message).to.contain('Missing argument. Requiring IUser');
+        expect(Cypress.log).to.be.calledWithMatch(sinon.match({ message: `{user: test}` }));
         done();
       });
 
-      cy.spy(Cypress, "log").log(false);
+      cy.spy(Cypress, 'log').log(false);
 
       //@ts-ignore
-      cy.deleteUser({ user: "test" });
+      cy.deleteUser({ user: 'test' });
     });
   });
 
-  context("getCurrentTenant", () => {
-    it("should get current tenant using c8yclient", function () {
-      cy.getAuth({ user: "admin", password: "mypassword", tenant: "t12345678" })
+  context('clearUserRoles', () => {
+    it('should clear user roles assigned to a user', () => {
+      const groups = {
+        references: [
+          {
+            group: {
+              id: 1,
+            },
+          },
+          {
+            group: {
+              id: 2,
+            },
+          },
+        ],
+      };
+
+      stubResponses([
+        new window.Response(
+          JSON.stringify({
+            groups,
+          }),
+          {
+            status: 200,
+            statusText: 'OK',
+          }
+        ),
+        new window.Response(null, {
+          status: 204,
+          statusText: 'OK',
+        }),
+        new window.Response(null, {
+          status: 204,
+          statusText: 'OK',
+        }),
+      ]);
+
+      const authUser = { user: 'admin', password: 'mypassword', tenant: 't12345678' };
+      const testUser = { userName: 'test', displayName: 'wewe' };
+
+      cy.getAuth(authUser)
+        .clearUserRoles(testUser)
+        .then(() => {
+          expectC8yClientRequest([
+            {
+              url: `${Cypress.config().baseUrl}/user/${authUser.tenant}/users/test`,
+              auth: authUser,
+              headers: { UseXBasic: true, accept: 'application/json' },
+            },
+            {
+              url: `${Cypress.config().baseUrl}/user/${authUser.tenant}/groups/${
+                groups.references[0].group.id
+              }/users/${testUser.userName}`,
+              auth: authUser,
+              headers: { UseXBasic: true, accept: 'application/json' },
+              method: 'DELETE',
+            },
+            {
+              url: `${Cypress.config().baseUrl}/user/${authUser.tenant}/groups/${
+                groups.references[1].group.id
+              }/users/${testUser.userName}`,
+              auth: authUser,
+              headers: { UseXBasic: true, accept: 'application/json' },
+              method: 'DELETE',
+            },
+          ]);
+        });
+    });
+  });
+
+  context('assignUserRoles', () => {
+    it('should assign roles to a user', () => {
+      const role1: Partial<IUserGroup> = {
+        id: 1,
+        name: 'role1',
+      };
+
+      const role2: Partial<IUserGroup> = {
+        id: 2,
+        name: 'role2',
+      };
+
+      const roles = [role1.name, role2.name];
+
+      stubResponses([
+        new window.Response(JSON.stringify(role1), {
+          status: 200,
+          statusText: 'OK',
+        }),
+        new window.Response(
+          JSON.stringify({
+            data: {
+              managedObject: {},
+            },
+          }),
+          {
+            status: 201,
+            statusText: 'OK',
+          }
+        ),
+        new window.Response(JSON.stringify(role2), {
+          status: 200,
+          statusText: 'OK',
+        }),
+        new window.Response(
+          JSON.stringify({
+            data: {
+              managedObject: {},
+            },
+          }),
+          {
+            status: 201,
+            statusText: 'OK',
+          }
+        ),
+      ]);
+
+      const authUser = { user: 'admin', password: 'mypassword', tenant: 't12345678' };
+      const testUser = { userName: 'test', displayName: 'wewe' };
+
+      cy.getAuth(authUser)
+        .assignUserRoles(testUser, ['role1', 'role2'])
+        .then(() => {
+          expectC8yClientRequest([
+            {
+              url: `${Cypress.config().baseUrl}/user/${authUser.tenant}/groupByName/${role1.name}`,
+              auth: authUser,
+              headers: { UseXBasic: true },
+            },
+            {
+              url: `${Cypress.config().baseUrl}/user/${authUser.tenant}/groups/${role1.id}/users`,
+              auth: authUser,
+              headers: {
+                UseXBasic: true,
+                accept: 'application/json',
+                'content-type': 'application/json',
+              },
+              body: JSON.stringify({
+                user: {
+                  self: `https://${Cypress.config().baseUrl}/user/${authUser.tenant}/users/${
+                    testUser.userName
+                  }`,
+                },
+              }),
+              method: 'POST',
+            },
+            {
+              url: `${Cypress.config().baseUrl}/user/${authUser.tenant}/groupByName/${role2.name}`,
+              auth: authUser,
+              headers: { UseXBasic: true },
+            },
+            {
+              url: `${Cypress.config().baseUrl}/user/${authUser.tenant}/groups/${role2.id}/users`,
+              auth: authUser,
+              headers: {
+                UseXBasic: true,
+                accept: 'application/json',
+                'content-type': 'application/json',
+              },
+              body: JSON.stringify({
+                user: {
+                  self: `https://${Cypress.config().baseUrl}/user/${authUser.tenant}/users/${
+                    testUser.userName
+                  }`,
+                },
+              }),
+              method: 'POST',
+            },
+          ]);
+        });
+    });
+  });
+
+  context('getCurrentTenant', () => {
+    it('should get current tenant using c8yclient', function () {
+      cy.getAuth({ user: 'admin', password: 'mypassword', tenant: 't12345678' })
         .getCurrentTenant()
         .then((response) => {
           expect(response.status).to.eq(200);
-          expect(response.body.name).to.eq("t1234");
+          expect(response.body.name).to.eq('t1234');
           expectC8yClientRequest({
             url: `${Cypress.config().baseUrl}/tenant/currentTenant`,
             auth: {
-              user: "admin",
-              password: "mypassword",
-              tenant: "t12345678",
+              user: 'admin',
+              password: 'mypassword',
+              tenant: 't12345678',
             },
             headers: { UseXBasic: true },
           });
         });
     });
 
-    it("should use client options", function () {
-      cy.getAuth({ user: "admin", password: "mypassword", tenant: "t12345678" })
+    it('should use client options', function () {
+      cy.getAuth({ user: 'admin', password: 'mypassword', tenant: 't12345678' })
         .getCurrentTenant({
-          baseUrl: "https://abc.def.com",
+          baseUrl: 'https://abc.def.com',
         })
         .then((response) => {
           expect(response.status).to.eq(200);
           expectC8yClientRequest({
             url: `https://abc.def.com/tenant/currentTenant`,
             auth: {
-              user: "admin",
-              password: "mypassword",
-              tenant: "t12345678",
+              user: 'admin',
+              password: 'mypassword',
+              tenant: 't12345678',
             },
             headers: { UseXBasic: true },
           });
@@ -138,41 +306,41 @@ describe("administration", () => {
     });
   });
 
-  context("getTenantId", () => {
-    it("should get tenant id using c8yclient", function () {
-      cy.getAuth({ user: "admin", password: "mypassword" })
+  context('getTenantId', () => {
+    it('should get tenant id using c8yclient', function () {
+      cy.getAuth({ user: 'admin', password: 'mypassword' })
         .getTenantId()
         .then((id) => {
-          expect(id).to.eq("t1234");
+          expect(id).to.eq('t1234');
           expect(window.fetchStub.callCount).to.equal(1);
         });
     });
 
-    it("should use tenant id from auth", function () {
-      cy.getAuth({ user: "admin", password: "mypassword", tenant: "t247652" })
+    it('should use tenant id from auth', function () {
+      cy.getAuth({ user: 'admin', password: 'mypassword', tenant: 't247652' })
         .getTenantId()
         .then((id) => {
-          expect(id).to.eq("t247652");
+          expect(id).to.eq('t247652');
           expect(window.fetchStub.callCount).to.equal(0);
         });
     });
 
-    it("should use tenant id from env variable", function () {
-      Cypress.env("C8Y_TENANT", "t232447652");
-      cy.getAuth({ user: "admin", password: "mypassword" })
+    it('should use tenant id from env variable', function () {
+      Cypress.env('C8Y_TENANT', 't232447652');
+      cy.getAuth({ user: 'admin', password: 'mypassword' })
         .getTenantId()
         .then((id) => {
-          expect(id).to.eq("t232447652");
+          expect(id).to.eq('t232447652');
           expect(window.fetchStub.callCount).to.equal(0);
         });
     });
 
-    it("should prefer tenant id from auth", function () {
-      Cypress.env("C8Y_TENANT", "t232447652");
-      cy.getAuth({ user: "admin", password: "mypassword", tenant: "t324678" })
+    it('should prefer tenant id from auth', function () {
+      Cypress.env('C8Y_TENANT', 't232447652');
+      cy.getAuth({ user: 'admin', password: 'mypassword', tenant: 't324678' })
         .getTenantId()
         .then((id) => {
-          expect(id).to.eq("t324678");
+          expect(id).to.eq('t324678');
           expect(window.fetchStub.callCount).to.equal(0);
         });
     });
