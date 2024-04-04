1be567bf4db59fcbf865faac9e846ecba83c9efc
Thomas Winkler <twi@softwareag.com>
2024-04-04 10:23:31 +0200

    oauthLogin and mount logging and error handling improvements.

--- src/lib/commands/mount.js
+++ src/lib/commands/mount.js
@@ -14,19 +14,33 @@ Cypress.Commands.add(
   "mount",
   { prevSubject: "optional" },
   (subject, component, options) => {
+    const consoleProps = {};
+    const logger = Cypress.log({
+      autoEnd: false,
+      name: "mount",
+      message: isClass(component) ? component.name : component,
+      consoleProps: () => consoleProps,
+    });
+
     const baseUrl = getBaseUrlFromEnv();
+    const auth = subject || getAuthOptionsFromEnv();
+    consoleProps.baseUrl = baseUrl;
+    consoleProps.auth = auth || null;
+    consoleProps.options = options;
+
     if (!baseUrl) {
+      logger.end();
       const error = new Error(
-        "No base URL configured. Use C8Y_BASEURL env variable for component testing."
+        "No base URL configured. cy.mount requires a base url. For component testing use C8Y_BASEURL env variable."
       );
       error.name = "C8yPactError";
       throw error;
     }
 
-    const auth = subject || getAuthOptionsFromEnv();
-    if (!auth) {
+    if (!auth || !auth.user || !auth.password) {
+      logger.end();
       const error = new Error(
-        "Missing authentication. cy.mount requires C8yAuthOptions."
+        "Missing authentication. cy.mount requires C8yAuthOptions with user and password."
       );
       error.name = "C8yPactError";
       throw error;
@@ -46,9 +60,13 @@ Cypress.Commands.add(
             useValue: fetchClient,
           });
           options.providers = providers;
+          consoleProps.providers = providers;
         }
       }
     };
+
+    consoleProps.isRecordingEnabled = Cypress.c8ypact.isRecordingEnabled();
+    consoleProps.strictMocking = Cypress.c8ypact.config.strictMocking;
 
     return (
       Cypress.c8ypact.isRecordingEnabled() ||
@@ -57,7 +75,17 @@ Cypress.Commands.add(
         : cy.wrap(auth)
     ).then((a) => {
       registerFetchClient(a);
+      logger.end();
       return mount(component, options);
     });
   }
 );
+
+function isClass(component) {
+  return (
+    component &&
+    typeof component === "function" &&
+    !!component.prototype &&
+    component.constructor != null
+  );
+}
