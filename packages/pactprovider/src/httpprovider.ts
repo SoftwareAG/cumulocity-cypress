ee344d019b89d5c148198b8c882d6118c7ded385
Thomas Winkler <twi@softwareag.com>
2024-04-14 17:57:17 +0200

    Added yargs cli for http provider.

--- packages/pactprovider/src/httpprovider.ts
+++ packages/pactprovider/src/httpprovider.ts
@@ -9,20 +9,24 @@ import {
   C8yDefaultPactPreprocessor,
 } from "cumulocity-cypress/node";
 
-const folder = process.env.C8Y_PACT_FOLDER;
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+
+const {
+  folder,
+  port,
+  baseUrl,
+  user,
+  password,
+  tenant,
+  staticRoot,
+  isRecordingEnabled,
+} = getArgs();
+
 if (!folder) {
   console.error("No pact folder specified. Please set C8Y_PACT_FOLDER.");
   process.exit(1);
 }
-
-const port = +(process.env.C8Y_HTTP_PORT || 3000);
-const baseUrl = process.env.C8Y_BASE_URL;
-const user = process.env.C8Y_BASE_USERNAME;
-const password = process.env.C8Y_BASE_PASSWORD;
-
-const tenant = process.env.C8Y_BASE_TENANT;
-const staticRoot = process.env.C8Y_STATIC_ROOT;
-const isRecordingEnabled = process.env.C8Y_PACT_MODE === "recording";
 
 console.log(`Using pact folder: ${folder}`);
 
@@ -68,3 +72,58 @@ if (staticRoot) {
     console.error("Error starting provider:", error);
   }
 })();
+
+function getEnvVar(name: string): string | undefined {
+  return (
+    process.env[name] ||
+    process.env[_.camelCase(name)] ||
+    process.env[`CYPRESS_${name}`]
+  );
+}
+
+function getArgs() {
+  return yargs(hideBin(process.argv))
+    .option("folder", {
+      type: "string",
+      description: "Folder recordings are loaded from and saved to.",
+      default: getEnvVar("C8Y_PACT_FOLDER"),
+    })
+    .option("port", {
+      type: "number",
+      description: "HTTP port the provider listens on.",
+      default: +(getEnvVar("C8Y_HTTP_PORT") || 3000),
+    })
+    .option("baseUrl", {
+      type: "string",
+      description:
+        "The Cumulocity URL REST requests are proxied and recorded from.",
+      default: getEnvVar("C8Y_BASE_URL"),
+    })
+    .option("user", {
+      type: "string",
+      description: "Set the username to login at baseUrl.",
+      default: getEnvVar("C8Y_BASE_USERNAME"),
+    })
+    .option("password", {
+      type: "string",
+      description: "Set the password to login at baseUrl.",
+      default: getEnvVar("C8Y_BASE_PASSWORD"),
+    })
+    .option("tenant", {
+      type: "string",
+      description: "Set the tenant of baseUrl.",
+      default: getEnvVar("C8Y_BASE_TENANT"),
+    })
+    .option("staticRoot", {
+      type: "string",
+      description: "Set the static root to serve static files from.",
+      default: getEnvVar("C8Y_STATIC_ROOT"),
+    })
+    .option("isRecordingEnabled", {
+      type: "boolean",
+      description: "Enable or disable recording",
+      default: getEnvVar("C8Y_PACT_MODE") === "recording",
+    })
+    .help()
+    .parseSync();
+}
