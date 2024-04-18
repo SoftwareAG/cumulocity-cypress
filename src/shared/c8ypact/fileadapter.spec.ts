283f7fbe92c55c43e8b7d81c7b484ca23ae3c9c0
Thomas Winkler <twi@softwareag.com>
2024-04-18 12:01:18 +0200

    Fixed implementation of createFolderRecursive in default file adapter. Added first tests.

---
+++ src/shared/c8ypact/fileadapter.spec.ts
@@ -0,0 +1,112 @@
+/// <reference types="jest" />
+
+import { C8yPactDefaultFileAdapter } from "./fileadapter";
+import { fs, vol } from "memfs";
+
+class C8yPactDefaultFileAdapterMock extends C8yPactDefaultFileAdapter {
+  createFolderRecursive(folderPath: string) {
+    return super.createFolderRecursive(folderPath);
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+jest.mock("fs", () => require("memfs").fs);
+
+const CWD = "/home/user/test";
+
+describe("C8yPactDefaultFileAdapter", () => {
+  beforeEach(() => {
+    jest.spyOn(process, "cwd").mockReturnValue(CWD);
+    vol.fromNestedJSON({
+      "cypress/test/c8ypact": {
+        "simpletest.json": Buffer.from(
+          JSON.stringify({
+            info: { id: "simpletest" },
+            records: [],
+            id: "simpletest",
+          })
+        ),
+      },
+    });
+  });
+
+  afterEach(() => {
+    vol.reset();
+  });
+
+  describe("setup", () => {
+    it("should create a new instance", () => {
+      const adapter = new C8yPactDefaultFileAdapter(CWD);
+      expect(adapter.getFolder()).toBe(CWD);
+    });
+
+    it("should create make absolute path for folder", () => {
+      const adapter = new C8yPactDefaultFileAdapter("cypress/test/c8ypact");
+      expect(adapter.getFolder()).toBe(`${CWD}/cypress/test/c8ypact`);
+    });
+  });
+
+  describe("loadPacts", () => {
+    it("should load pact object", () => {
+      const adapter = new C8yPactDefaultFileAdapter("cypress/test/c8ypact");
+      const pacts = adapter.loadPacts();
+      expect(pacts).toBeDefined();
+      expect(pacts["simpletest"]).toBeDefined();
+      expect(pacts["simpletest"].info.id).toBe("simpletest");
+    });
+
+    it("should return empty object if folder does not exist", () => {
+      const adapter = new C8yPactDefaultFileAdapter(`${CWD}/23`);
+      const pacts = adapter.loadPacts();
+      expect(pacts).toEqual({});
+    });
+  });
+
+  describe("createFolderRecursive", () => {
+    it("should create folder recursively from absolute path", () => {
+      const adapter = new C8yPactDefaultFileAdapterMock(CWD);
+      const folderPath = `${CWD}/folder1/folder2`;
+
+      expect(fs.existsSync(folderPath)).toBe(false);
+      const result = adapter.createFolderRecursive(folderPath);
+      expect(result).toBe(`${CWD}/folder1/folder2`);
+
+      // check mock-fs folder has been created
+      expect(fs.existsSync(folderPath)).toBe(true);
+    });
+
+    it("should create recursively from relative path", () => {
+      const adapter = new C8yPactDefaultFileAdapterMock("cypress/test/c8ypact");
+      const folderPath = `${CWD}/cypress2/test2/c8ypact2/`;
+
+      expect(fs.existsSync(folderPath)).toBe(false);
+      const result = adapter.createFolderRecursive(folderPath);
+      expect(fs.existsSync(folderPath)).toBe(true);
+      expect(result?.endsWith(`cypress2/test2/c8ypact2`)).toBe(true);
+    });
+
+    it("should return undefined if absolute path already exists", () => {
+      const adapter = new C8yPactDefaultFileAdapterMock(CWD);
+      const folderPath = `${CWD}/cypress/test/c8ypact`;
+
+      expect(fs.existsSync(folderPath)).toBe(true);
+      const result = adapter.createFolderRecursive(folderPath);
+      expect(result).toBeUndefined();
+    });
+
+    it("should return undefined if relative path already exists", () => {
+      const adapter = new C8yPactDefaultFileAdapterMock("cypress/test/c8ypact");
+      const result = adapter.createFolderRecursive("cypress/test/c8ypact");
+      expect(result).toBeUndefined();
+    });
+
+    it("should return undefined if folder path is not a string", () => {
+      const adapter = new C8yPactDefaultFileAdapterMock(CWD);
+      const folderPath: string | undefined = undefined;
+      const result = adapter.createFolderRecursive(folderPath as any);
+      expect(result).toBeUndefined();
+      const result2 = adapter.createFolderRecursive({} as any);
+      expect(result2).toBeUndefined();
+    });
+  });
+});
--- src/shared/c8ypact/fileadapter.ts
+++ src/shared/c8ypact/fileadapter.ts
@@ -4,6 +4,10 @@ import * as glob from "glob";
 import debug from "debug";
 import { C8yPact, C8yPactSaveKeys } from "./c8ypact";
 
+import lodash1 from "lodash";
+import * as lodash2 from "lodash";
+const _ = lodash1 || lodash2;
+
 /**
  * Using C8yPactFileAdapter you can implement your own adapter to load and save pacts using any format you want.
  * This allows loading pact objects from different sources, such as HAR files, pact.io, etc.
@@ -43,7 +47,9 @@ const log = debug("c8y:plugin:fileadapter");
 export class C8yPactDefaultFileAdapter implements C8yPactFileAdapter {
   folder: string;
   constructor(folder: string) {
-    this.folder = folder;
+    this.folder = path.isAbsolute(folder)
+      ? folder
+      : this.toAbsolutePath(folder);
   }
 
   getFolder(): string {
@@ -79,7 +85,7 @@ export class C8yPactDefaultFileAdapter implements C8yPactFileAdapter {
   }
 
   savePact(pact: C8yPact | Pick<C8yPact, C8yPactSaveKeys>): void {
-    this.createFolderRecursive(this.folder, true);
+    this.createFolderRecursive(this.folder);
     const file = path.join(this.folder, `${pact.id}.json`);
     log(`savePact() - ${file}`);
     fs.writeFileSync(
@@ -101,7 +107,7 @@ export class C8yPactDefaultFileAdapter implements C8yPactFileAdapter {
     const filePath = path.join(this.folder, `${id}.json`);
     if (fs.existsSync(filePath)) {
       fs.unlinkSync(filePath);
-      log(`deletePact() - deletaed ${filePath}`);
+      log(`deletePact() - deleted ${filePath}`);
     } else {
       log(`deletePact() - ${filePath} does not exist`);
     }
@@ -142,19 +148,26 @@ export class C8yPactDefaultFileAdapter implements C8yPactFileAdapter {
     return pacts.map((pact) => JSON.parse(pact));
   }
 
-  protected createFolderRecursive(f: string, absolutePath: boolean) {
+  protected createFolderRecursive(f: string) {
     log(`createFolderRecursive() - ${f}`);
-    const parts = f?.split(path.sep);
-    parts.forEach((part, i) => {
-      let currentPath = path.join(...parts.slice(0, i + 1));
-      if (absolutePath) {
-        currentPath = path.join(path.sep, currentPath);
-      }
-      if (!fs.existsSync(currentPath)) {
-        log(`createFolderRecursive() - creating ${currentPath}`);
-        fs.mkdirSync(currentPath);
-      }
-    });
+    if (!f || !_.isString(f)) return undefined;
+
+    const absolutePath = !path.isAbsolute(f) ? this.toAbsolutePath(f) : f;
+    if (f !== absolutePath) {
+      log(`createFolderRecursive() - resolved ${f} to ${absolutePath}`);
+    }
+
+    if (fs.existsSync(f)) return undefined;
+
+    const result = fs.mkdirSync(absolutePath, { recursive: true });
+    if (result) {
+      log(`createFolderRecursive() - created ${absolutePath}`);
+    }
+    return result;
+  }
+
+  protected toAbsolutePath(f: string) {
+    return path.isAbsolute(f) ? f : path.resolve(process.cwd(), f);
   }
 
   protected isNodeError<T extends new (...args: any) => Error>(
