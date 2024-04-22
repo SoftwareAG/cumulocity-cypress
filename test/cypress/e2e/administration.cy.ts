d292b70b7dcac67c75905a4b0ff4b70b9c507905
Thomas Winkler <twi@softwareag.com>
2024-04-22 22:50:32 +0200

    Fixed issue in administration cypress test. Missing file.

--- test/cypress/e2e/administration.cy.ts
+++ test/cypress/e2e/administration.cy.ts
@@ -85,15 +85,12 @@ describe("administration", () => {
     });
 
     it("throws error for missing user and logs username", (done) => {
+      const spy = cy.spy(Cypress, "log").log(false);
+
       Cypress.once("fail", (err) => {
         expect(err.message).to.contain("Missing argument. Requiring IUser");
-        expect(Cypress.log).to.be.calledWithMatch(
-          sinon.match({ message: `{user: test}` })
-        );
         done();
       });
-
-      cy.spy(Cypress, "log").log(false);
 
       //@ts-expect-error
       cy.deleteUser({ user: "test" });
