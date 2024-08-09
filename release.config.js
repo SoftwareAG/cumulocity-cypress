75b6fd425a829662187332fdfb92128a329d7888
Thomas Winkler <twi@softwareag.com>
2024-08-09 23:10:38 +0200

    feat(release): Create CHANGELOG.md and npm release

--- release.config.js
+++ release.config.js
@@ -4,22 +4,22 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    // [
-    //   "@semantic-release/npm",
-    //   {
-    //     npmPublish: false,
-    //     pkgRoot: "dist/",
-    //   },
-    // ],
-    // "@semantic-release/github",
-    // [
-    //   "@semantic-release/git",
-    //   {
-    //     assets: ["CHANGELOG.md", "package.json"],
-    //     message:
-    //       "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-    //   },
-    // ],
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+        pkgRoot: "dist/",
+      },
+    ],
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
     [
       "@semantic-release/exec",
       {
