fb3a4c66c1c87bc4f9941ef5e31257293cbedbcd
Thomas Winkler <twi@softwareag.com>
2024-08-11 16:39:43 +0200

    fix(release): Fix release branch pattern on main

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ["main", "release/v+([0-9])?(.{+([0-9]),x}).x"],
+  branches: ["main", "release/v+([0-9])?(.{+([0-9]),x})"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
